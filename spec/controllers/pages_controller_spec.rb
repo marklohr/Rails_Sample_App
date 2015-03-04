e23ddc07a690445be658b118f5ec6d8eb8625103
Mark Lohr <markuslohr@gmail.com>
2015-03-04 00:18:03 -0500

    more TDD

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe PagesController, type: :controller do
-
+  render_views
+  
   describe "GET #home" do
     it "returns http success" do
       get :home
       expect(response).to have_http_status(:success)
+    end
+
+    it "should have the right title" do
+      get :home
+      expect(response).to have_selector("title",
+                        :content => "Ruby on Rails Tutorial Sample App | Home")
     end
   end
 
